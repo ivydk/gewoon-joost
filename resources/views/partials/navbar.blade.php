b7f29de84f8c59fda7ef33dc7d4aedc4fd7efd1c
Kevin van Herwijnen <76037700+kvherwijnen@users.noreply.github.com>
2022-07-06 06:40:11 +0200

    fixed error + deleted code

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -10,38 +10,4 @@
             <span aria-hidden="true"></span>
         </a>
     </div>
-
-    <div id="navbarBasicExample" class="navbar-menu">
-        <div class="navbar-start">
-            <a class="navbar-item">
-                Home
-            </a>
-
-            <a class="navbar-item" href="{{ url('upload') }}">
-                Upload
-            </a>
-
-{{--            <div class="navbar-item has-dropdown is-hoverable">--}}
-{{--                <a class="navbar-link" href="{{ url('upload') }}">--}}
-{{--                    upload--}}
-{{--                </a>--}}
-
-{{--                <div class="navbar-dropdown">--}}
-{{--                    <a class="navbar-item">--}}
-{{--                        About--}}
-{{--                    </a>--}}
-{{--                    <a class="navbar-item">--}}
-{{--                        Jobs--}}
-{{--                    </a>--}}
-{{--                    <a class="navbar-item">--}}
-{{--                        Contact--}}
-{{--                    </a>--}}
-{{--                    <hr class="navbar-divider">--}}
-{{--                    <a class="navbar-item">--}}
-{{--                        Report an issue--}}
-{{--                    </a>--}}
-{{--                </div>--}}
-{{--            </div>--}}
-        </div>
-    </div>
 </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,15 +5,15 @@
         <div class="container">
             <form action="{{ url('store-input-fields') }}" method="POST">
                 @csrf
-                @if ($errors->any())
-                    <div class="alert alert-danger" role="alert">
-                        <ul>
-                            @foreach ($errors->all() as $error)
-                                <li>{{ $error }}</li>
-                            @endforeach
-                        </ul>
-                    </div>
-                @endif
+{{--                @if ($errors->any())--}}
+{{--                    <div class="alert alert-danger" role="alert">--}}
+{{--                        <ul>--}}
+{{--                            @foreach ($errors->all() as $error)--}}
+{{--                                <li>{{ $error }}</li>--}}
+{{--                            @endforeach--}}
+{{--                        </ul>--}}
+{{--                    </div>--}}
+{{--                @endif--}}
                 @if (Session::has('success'))
                     <div class="alert alert-success text-center">
                         <p>{{ Session::get('success') }}</p>
