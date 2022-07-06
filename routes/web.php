ff8f4a06c15ae60f77ca211849009596447d4393
Kevin van Herwijnen <76037700+kvherwijnen@users.noreply.github.com>
2022-07-06 06:49:41 +0200

    sleep

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//route:
 Route::get('/section-form', [SectionsController::class, 'index']);
 Route::post('/store-input-fields', [SectionsController::class, 'store']);
 
